e4cd84330d1d9373d903deadabecf698de8100d1
aleciofilho <alecio_filho@hotmail.com>
2022-02-21 15:30:13 -0300

    c05-01-02. Refactoring: now tests work.

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -6,8 +6,16 @@ class QuestionsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Questions"
   # end
-  test "visiting /ask renders the form" do
+
+  test 'visiting /ask renders the form' do
     visit ask_url
-    assert_selector "p", text: "Ask your coach anything"
+    assert_selector 'p', text: 'Ask your coach anything'
+  end
+
+  test 'saying Hello yields a grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'Hello'
+    click_on 'Ask'
+    assert_text "I don't care, get dressed and go to work!"
   end
 end
