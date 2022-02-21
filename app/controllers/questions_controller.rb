10a92d6e096097cf57c2337b8be16ebd428e8f53
aleciofilho <alecio_filho@hotmail.com>
2022-02-21 14:58:20 -0300

    c05-01-02. Refactoring: logic from view to controller. Still can't test.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,13 @@
 class QuestionsController < ApplicationController
   def ask; end
-  def answer; end
+  def answer
+    @question = params[:question]
+    @answer = if @question.downcase == 'I am going to work'.downcase
+                'Great!'
+              elsif @question.ends_with?('?')
+                'Silly question, get dressed and go to work!'
+              else
+                "I don't care, get dressed and go to work!"
+              end
+  end
 end
